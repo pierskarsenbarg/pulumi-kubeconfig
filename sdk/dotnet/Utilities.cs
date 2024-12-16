5bb946ad3892dd624d414548d0147c89ebcb3c22
Piers Karsenbarg <piers@pulumi.com>
2024-12-16 09:21:17 +0000

    Building sdks

--- sdk/dotnet/Utilities.cs
+++ sdk/dotnet/Utilities.cs
@@ -57,6 +57,14 @@ namespace PiersKarsenbarg.Pulumi.Kubeconfig
             return dst;
         }
 
+        public static global::Pulumi.InvokeOutputOptions WithDefaults(this global::Pulumi.InvokeOutputOptions? src)
+        {
+            var dst = src ?? new global::Pulumi.InvokeOutputOptions{};
+            dst.Version = src?.Version ?? Version;
+            dst.PluginDownloadURL = src?.PluginDownloadURL ?? "github://api.github.com/pierskarsenbarg/pulumi-kubeconfig";
+            return dst;
+        }
+
         private readonly static string version;
         public static string Version => version;
 
